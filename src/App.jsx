d676f0a75efa956cefc402f97f94b34ee0ed1254
robiulalamdev <robiulalam762400@gmail.com>
2024-11-07 01:09:24 +0600

    email desgin udpate, loading, and copy functionadd

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,137 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./App.css";
+import { I_CopyButton, I_CopyTick, iSpinner } from "./lib/icons";
 
 function App() {
-  const [emails, setEmails] = useState([]);
+  const [emails, setEmails] = useState([
+    // "robiulalamdev@gmail.com",
+    // "robiulalamdev@gmail.com",
+    // "robiulalamdev@gmail.com",
+    // "robiulalamdev@gmail.com",
+  ]);
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleScrapeEmails = async () => {
-    let [tab] = await chrome.tabs.query({
-      active: true,
-      currentWindow: true,
-    });
+    try {
+      setIsLoading(true);
+      let [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
 
-    const response = await chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: () => {
-        const emailRegex = /\b[\w.-]+@[\w.-]+\.[A-Za-z]{2,}\b/g;
-        const emailsScrapped = document.body.innerHTML.match(emailRegex);
-        return emailsScrapped;
-      },
-    });
+      const response = await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+          const emailRegex = /\b[\w.-]+@[\w.-]+\.[A-Za-z]{2,}\b/g;
+          const emailsScrapped = document.body.innerHTML.match(emailRegex);
+          return emailsScrapped;
+        },
+      });
 
-    if (response?.length > 0 && response[0]?.result) {
-      if (response[0]?.result?.length > 0) {
-        setEmails([...response[0].result]);
+      if (response?.length > 0 && response[0]?.result) {
+        if (response[0]?.result?.length > 0) {
+          setEmails([...response[0].result]);
+        }
       }
+    } catch (error) {
+      setIsLoading(false);
     }
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
   };
 
   return (
-    <div className="w-[400px] h-fit py-6 px-6 bg-white rounded-lg shadow-md">
-      <button
-        onClick={() => handleScrapeEmails()}
-        className="w-full py-2 px-4 mb-4 text-white bg-blue-600 hover:bg-blue-700 font-semibold rounded-md shadow transition duration-200"
-      >
-        Scrape Emails
-      </button>
-      <div className="flex justify-center items-center w-full overflow-y-auto h-[400px] max-h-[400px] bg-gray-50 mt-4 rounded-lg shadow-inner">
-        <div className="w-full max-w-lg p-4">
-          <h2 className="text-xl font-semibold text-gray-800 mb-4 text-center">
-            Email List
-          </h2>
-          <ul className="bg-white rounded-lg shadow-md divide-y divide-gray-200">
-            {emails.map((email, index) => (
-              <li
-                key={index}
-                className="flex items-center justify-between px-4 py-3 hover:bg-gray-100 transition duration-150"
-              >
-                <span className="text-gray-700">{email}</span>
-                <button>Copy</button>
-              </li>
-            ))}
-          </ul>
+    <div className="w-[450px] h-fit py-6 px-6 bg-white rounded-lg shadow-md mx-auto">
+      <div className="w-full h-[400px] max-h-[400px] bg-gray-50 mt-4 rounded-lg shadow-inner p-[20px]">
+        <div className="w-full h-full overflow-y-auto">
+          {isLoading && (
+            <ul className="bg-white rounded-lg shadow-md divide-y divide-gray-200 animate-pulse">
+              <EmailLoader />
+              <EmailLoader />
+              <EmailLoader />
+              <EmailLoader />
+              <EmailLoader />
+              <EmailLoader />
+              <EmailLoader />
+            </ul>
+          )}
+
+          {!isLoading && (
+            <ul className="bg-white rounded-lg shadow-md divide-y divide-gray-200">
+              {emails.map((email, index) => (
+                <SingleEmail key={index} email={email} />
+              ))}
+            </ul>
+          )}
+
+          {!isLoading && emails.length === 0 && (
+            <div className="flex justify-center items-center w-full h-full">
+              <h1 className="text-black text-[16px] font-medium">No data</h1>
+            </div>
+          )}
         </div>
+      </div>
+      <div className="mt-[20px]">
+        <button
+          disabled={isLoading}
+          onClick={() => handleScrapeEmails()}
+          className="w-full py-2 px-4 mb-4 text-white bg-blue-600 hover:bg-blue-700 font-semibold rounded-md shadow transition duration-200 flex justify-center items-center gap-2"
+        >
+          {isLoading && (
+            <div className="max-w-[20px] max-h-[20px] flex justify-center items-center">
+              {iSpinner}
+            </div>
+          )}
+          Scrape Emails
+        </button>
       </div>
     </div>
   );
 }
+
+const EmailLoader = () => {
+  return (
+    <li className="flex items-center justify-between px-4 py-3">
+      <div className="h-4 bg-gray-200 rounded w-1/2"></div>
+      <div className="h-6 bg-gray-200 rounded w-16"></div>
+    </li>
+  );
+};
+
+const SingleEmail = ({ email = "" }) => {
+  const [copied, setCopied] = useState(false);
+
+  const copyTextToClipboard = (text) => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 3000);
+      })
+      .catch((err) => {
+        console.error("Failed to copy text: ", err);
+      });
+  };
+  return (
+    <li className="flex items-center justify-between px-4 py-3 hover:bg-gray-100 transition duration-150">
+      <span className="flex-grow w-full text-black oneLine">{email}</span>
+      {copied ? (
+        <button className="text-green-500 w-[14px] h-[14px] flex justify-center items-center">
+          {I_CopyTick}
+        </button>
+      ) : (
+        <button
+          onClick={() => copyTextToClipboard(email)}
+          className="w-[14px] h-[14px] flex justify-center items-center text-black"
+        >
+          {I_CopyButton}
+        </button>
+      )}
+    </li>
+  );
+};
 
 export default App;
---
+++ src/lib/icons/index.jsx
@@ -0,0 +1,62 @@
+export const iSpinner = (
+  <svg
+    stroke="currentColor"
+    fill="none"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="animate-spin text-center"
+    height="100%"
+    width="100%"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <line x1="12" y1="2" x2="12" y2="6"></line>
+    <line x1="12" y1="18" x2="12" y2="22"></line>
+    <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
+    <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line>
+    <line x1="2" y1="12" x2="6" y2="12"></line>
+    <line x1="18" y1="12" x2="22" y2="12"></line>
+    <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line>
+    <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
+  </svg>
+);
+
+export const I_CopyButton = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
+  >
+    <g clipPath="url(#clip0_1_536)">
+      <path
+        d="M8 0.5H2C1.45 0.5 1 0.95 1 1.5V8.5H2V1.5H8V0.5ZM9.5 2.5H4C3.45 2.5 3 2.95 3 3.5V10.5C3 11.05 3.45 11.5 4 11.5H9.5C10.05 11.5 10.5 11.05 10.5 10.5V3.5C10.5 2.95 10.05 2.5 9.5 2.5ZM9.5 10.5H4V3.5H9.5V10.5Z"
+        fill="currentColor"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_1_536">
+        <rect width="12" height="12" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export const I_CopyTick = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={4.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m4.5 12.75 6 6 9-13.5"
+    />
+  </svg>
+);
