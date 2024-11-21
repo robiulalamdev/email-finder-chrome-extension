9315cdb27d851a6e033df5357a5a7de6064aa4e6
robiulalamdev <robiulalam762400@gmail.com>
2024-11-21 23:00:49 +0600

    Added Material Tailwind React library, updated App.jsx to use Button component, and modified tailwind.config.js to include Material Tailwind React configurations.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ThemeProvider } from "@material-tailwind/react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <ThemeProvider>
     <App />
-  </StrictMode>,
-)
+  </ThemeProvider>
+  // </StrictMode>,
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      maxWidth: {
+        theme: "1920px",
+      },
+      colors: {},
+      fontFamily: {},
+    },
+  },
+  variants: {
+    extend: {
+      display: ["group-focus", "group-hover"],
+    },
   },
   plugins: [],
-};
+});
